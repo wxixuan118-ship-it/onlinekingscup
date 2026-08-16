3decb377e7a89cb61c6b07d8ae439637caafc7f6
Viki WU <270457286@qq.com>
2026-08-16 18:44:53 +0800

    Deploy static Online Kings Cup site

---
+++ partydeck.tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./party-games-presale.html"],
+  theme: {
+    extend: {
+      colors: {
+        ink: "#111827",
+        violet: "#6d4aff",
+        lemon: "#ffd84d",
+        cloud: "#f6f7fb"
+      },
+      fontFamily: {
+        sans: ["Inter", "ui-sans-serif", "system-ui", "sans-serif"]
+      },
+      boxShadow: {
+        soft: "0 18px 60px rgba(28, 25, 66, .10)"
+      }
+    }
+  },
+  plugins: []
+};
